d1ce161ca5562140a23e9c30ade3cd9ea89553d1
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-01-13 14:31:36 -0300

    mudança na função main para tirar a repetição de cod

--- getImagens.py
+++ getImagens.py
@@ -1,4 +1,3 @@
-import pandas as pd 
 import requests
 import json
 import asyncio
@@ -8,21 +7,18 @@ r = requests.get('https://conteudo.bannet.com.br/api/collections/get/psOperadora
 rJson = r.json()['entries']
 
 async def main(value):
-    browser = await launch(headless=True, ignoreHTTPSErrors=True, args=['--no-sandbox']) 
-    page = await browser.newPage()
-    await page.goto("https://www.google.com/search?q=operadora-"+value+"-logo&hl=pt-BR&tbm=isch",{'delay': 2000})
-    element = await page.querySelector('html body div div div div div div div div div div div div div div a img')
-    print(element)
+    element = None
     while(element==None):
-        await browser.close()
-        await asyncio.sleep(5)
         browser = await launch(headless=True, ignoreHTTPSErrors=True, args=['--no-sandbox']) 
         page = await browser.newPage()
         await page.goto("https://www.google.com/search?q=operadora-"+value+"-logo&hl=pt-BR&tbm=isch",{'delay': 2000})
         element = await page.querySelector('html body div div div div div div div div div div div div div div a img')
+        await asyncio.sleep(5)
         print(element)
     await element.screenshot({'path': './imagens/'+value+'.png'})
     await browser.close()
 for item in rJson:
-        asyncio.get_event_loop().run_until_complete(main(item['url']))
+    if(item['imagem']==''):
+        # asyncio.get_event_loop().run_until_complete(main(item['url']))
+        print(item['nome'])
         
